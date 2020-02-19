91c9535585eea7d318cfcf56df72233756265f4a
Karolina Sienkiewicz <sienkiewicz2k@gmail.com>
2020-02-19 17:37:57 -0500

    Update original benchamrk

--- benchmark/run_benchmark.R
+++ benchmark/run_benchmark.R
@@ -16,8 +16,8 @@ MC.CORES <- 25
 VARS.FNAME <- "set_vars.sh"
 RANDOM.SEED <- 42
 
-ALGORITHM.NAMES = c('snf', 'lracluster', 'pins', 'mcca', 'nemo') #sumo
-ALGORITHM.DISPLAY.NAMES = as.list(c('SNF', 'LRAcluster', 'PINS', 'MCCA', 'NEMO')) #SUMO
+ALGORITHM.NAMES = c('snf', 'lracluster', 'pins', 'mcca', 'nemo', 'sumo')
+ALGORITHM.DISPLAY.NAMES = as.list(c('SNF', 'LRAcluster', 'PINS', 'MCCA', 'NEMO', 'SUMO'))
 names(ALGORITHM.DISPLAY.NAMES) = ALGORITHM.NAMES
 
 OMIC.SUBSETS = list('multi_omics')
@@ -25,24 +25,16 @@ names(OMIC.SUBSETS) = c('all')
 
 # run all tools
 load.libraries()
-
-repetitions <- 10
-for (i in 1:repetitions){
-  RESULTS.DIR.PATH = paste("results", i, sep = "_")
-  SUMO.FILES.DIR <- paste("sumo_files", i, sep="_")
-  RANDOM.SEED <- i
+run.benchmark()
   
-  run.benchmark()
-  
-  # move sumo files
-  if ('sumo' %in% ALGORITHM.NAMES){
-    move.sumo.files()
-    ALGORITHM.NAMES = c(ALGORITHM.NAMES,paste0('sumo', 2:MAX.NUM.CLUSTERS))
-    ALGORITHM.DISPLAY.NAMES = as.list(c(unlist(ALGORITHM.DISPLAY.NAMES),paste0('SUMO', 2:MAX.NUM.CLUSTERS)))
-    names(ALGORITHM.DISPLAY.NAMES) = ALGORITHM.NAMES
-  }
-  
-  # calculate empirical survival and enriched clinical labels
-  results <- analyze.benchmark()
-  perform.all.analyses(results)
+# move sumo files
+if ('sumo' %in% ALGORITHM.NAMES){
+  move.sumo.files()
+  ALGORITHM.NAMES = c(ALGORITHM.NAMES,paste0('sumo', 2:MAX.NUM.CLUSTERS))
+  ALGORITHM.DISPLAY.NAMES = as.list(c(unlist(ALGORITHM.DISPLAY.NAMES),paste0('SUMO', 2:MAX.NUM.CLUSTERS)))
+  names(ALGORITHM.DISPLAY.NAMES) = ALGORITHM.NAMES
 }
+  
+# calculate empirical survival and enriched clinical labels
+results <- analyze.benchmark()
+perform.all.analyses(results)
