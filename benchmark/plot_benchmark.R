5973d728efbd4d1a921996480d7d807730846426
Karolina Sienkiewicz <sienkiewicz2k@gmail.com>
2020-12-28 15:08:46 -0500

    Update benchmark plotting

--- benchmark/plot_benchmark.R
+++ benchmark/plot_benchmark.R
@@ -1,10 +1,16 @@
 library(tidyverse)
 library(ggsci)
+library(cowplot)
+library(gridExtra)
+library(grid)
+library(ggpubr)
+
+get.tables.dir.path <- function(){
+  return("tables")
+}
 
 clin <- read_csv(file.path(get.tables.dir.path(), "clinical_multi_omics.csv"))
 surv <- read_csv(file.path(get.tables.dir.path(), "survival_multi_omics.csv"))
-
-###
 
 all_surv <- surv %>% mutate(tool=X1) %>% select(-X1) %>% gather(cancer, survival, -tool)
 all_clin <- clin %>% mutate(tool=X1) %>% select(-X1) %>% gather(cancer, clin, -tool)
@@ -16,23 +22,10 @@ all_surv$is_sumo <- grepl("SUMO",all_surv$tool)
 sumo_surv <- all_surv %>% filter(is_sumo == TRUE) %>% separate(tool, c("tmp", "k"), sep = "SUMO") %>% select(-tmp) %>% mutate(tool=paste0("SUMO",k))
 
 sumo_data <- sumo_surv %>% full_join(sumo_clin) %>% filter(k != "")
-
 sumo_data
 
-sumo_data %>%
-  mutate(tool="SUMO") %>%
-  ggplot() + geom_point(aes(x=survival,y=clin,color=as.factor(k)), size=3) + facet_wrap(cancer~., scales = "free", ncol=4) +
-  theme_bw() + labs(color="k") + geom_vline(xintercept = -log10(0.05), color="red") + ylab('# enriched clinical parameters') +
-  xlab("-log10(logrank pvalue)") + theme(legend.position = "bottom", axis.text = element_text(size=12),
-        strip.text.x = element_text(size = 12, face="bold"), axis.title = element_text(size=12),
-        legend.text=element_text(size=12))
-
-sumo_plot <- last_plot()
-
-###
 cancers <- c("AML", "BIC", "COAD", "GBM", "KIRC", "LIHC", "LUSC", "SKCM", "OV", "SARC")
 selected <- c(12, 13, 7, 10, 6, 2, 9, 7, 9, 6)
-
 selected_sumo <- sumo_data %>% inner_join(tibble(k=as.character(selected), cancer=cancers)) %>% mutate(tool="SUMO")
 selected_sumo
 
@@ -40,17 +33,52 @@ clin_data <- all_clin %>% filter(is_sumo == FALSE)
 surv_data <- all_surv %>% filter(is_sumo == FALSE)
 
 data <- clin_data %>% full_join(surv_data) %>% full_join(selected_sumo %>% select(-k))
-data
-data %>%
-  ggplot() + geom_point(aes(x=survival, y=clin, color=tool, shape= is_sumo), size = 3) +
-  facet_wrap(cancer~., scales="free", ncol=4) + xlab("-log10(logrank pvalue)") + ylab("# enriched clinical parameters") +
-  labs(color="") + theme_bw() + geom_vline(xintercept = -log10(0.05), color="red") + scale_shape(guide="none") +
-  theme(legend.position = "bottom", axis.text = element_text(size=12), strip.text.x = element_text(size = 12, face="bold"),
-        axis.title = element_text(size=12), legend.text=element_text(size=12)) + scale_color_npg()
-
-results_plot <- last_plot()
+data %>% filter(tool == "iClusterBayes")
 
 ###
+data <- read_tsv("benchmark_results_selected_0.2.5.tsv")
+data <- data %>% mutate(tool = as.factor(tool), is_sumo = as.logical(is_sumo))
+data$tool <- factor(data$tool, levels = c("NEMO", "SUMO","LRAcluster", "MCCA", 
+                                          "PINSPlus", "SNF", "iClusterBayes"))
 
 data %>% mutate(sig_surv = survival >= -log10(0.05), sig_clin = clin > 0) %>% filter(sig_surv == TRUE | sig_clin == TRUE) %>%
-  group_by(tool) %>% summarise(clinincal=sum(sig_clin), survival=sum(sig_surv))
+  group_by(tool) %>% summarise(clinical=sum(sig_clin), survival=sum(sig_surv)) %>%
+  select(tool, survival, clinical)
+stats <- .Last.value
+stats <- tibble(tool=c('LRAcluster', 'MCCA', 'NEMO', 'PINSPlus', 'SNF', 'iClusterBayes', 'SUMO')) %>%
+  mutate(tool=as.factor(tool)) %>%
+  left_join(stats)
+
+colnames(stats) <- c("Method", "Number of cancers\n with differential survival", "Number of cancers\n with clinical enrichment")
+
+tt <- ttheme_default(core=list(
+  fg_params=list(fontface=c(rep("plain", 6), "bold")),
+  bg_params = list(fill=c("grey95", "grey90","grey95", "grey90","grey95", "grey90","grey60"),
+                   alpha = rep(c(1,0.5), each=6))
+), base_size = 10.5)
+
+figB <- tableGrob(stats, rows=NULL, theme=tt)
+
+ggplot(data, aes(survival, clin, color=tool, shape=is_sumo)) + 
+  geom_point(size=3) + 
+  scale_y_continuous(breaks=c(0,1,2,3,4), 
+                     labels=c("0","1","2","3","4"),
+                     limits=c(0,NA)) +
+  xlab(expression(-log[10](`logrank p-value`))) + 
+  ylab("number of enriched clinical parameters") +
+  facet_wrap(cancer ~ ., ncol=4) +
+  geom_vline(xintercept=-log10(0.05), color="red") + 
+  theme_bw() + 
+  theme(legend.position=c(0.75,0.15), 
+        legend.direction = "horizontal",
+        panel.grid.minor.y = element_blank()) +
+  guides(shape=FALSE, 
+         color=guide_legend(override.aes = list(shape=c(16,17,16,16,16,16,16), size=3), nrow=3, byrow = TRUE)) +
+  labs(color="", shape="") +
+  scale_color_npg() 
+
+figA <- last_plot()
+
+cairo_pdf("benchmark_results.pdf", width=7, height=7.5)
+ggarrange(figA, figB, labels = c('A','B'), ncol=1, heights = c(2,1))
+dev.off()
