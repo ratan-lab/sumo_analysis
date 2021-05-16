c9294d8992014e81c2f56dc4aaae2c4ca5c9daab
Karolina Sienkiewicz <sienkiewicz2k@gmail.com>
2021-05-15 20:42:02 -0400

    Update benchmark eval, add CIMLR

--- benchmark/run_benchmark_eval.R
+++ benchmark/run_benchmark_eval.R
@@ -4,13 +4,11 @@ source('SUMO.R')
 source("benchmark.R")
 
 # SUMO params
-SUMO.FILES.DIR <- "sumo_files"
 SUMO.PATH <- "sumo"
-MAX.NUM.CLUSTERS <- 15
 
 # benchmark params
 DATASETS.PATH = "data"
-RESULTS.DIR.PATH = "results"
+RESULTS.DIR.PATH.MAIN = "results_eval"
 CLINICAL.PARAMS.DIR = "data/clinical"
 MC.CORES <- 25
 VARS.FNAME <- "set_vars.sh"
@@ -18,17 +16,21 @@ RANDOM.SEED <- 42
 OMIC.SUBSETS = list('multi_omics')
 names(OMIC.SUBSETS) = c('all')
 
+SUBTYPES.DATA = list(list(name='gbm', only.primary=T, is.rna.seq=F, is.mirna.seq=F, display.name='GBM'))
+MAX.NUM.CLUSTERS <- 15
+
 # run all tools
 load.libraries()
+dir.create(RESULTS.DIR.PATH.MAIN)
 
 repetitions <- 10
 for (i in 1:repetitions){
-  RESULTS.DIR.PATH = paste("results", i, sep = "_")
-  SUMO.FILES.DIR <- paste("sumo_files", i, sep="_")
+  RESULTS.DIR.PATH = file.path(RESULTS.DIR.PATH.MAIN, paste("results", i, sep = "_"))
+  SUMO.FILES.DIR <- file.path(RESULTS.DIR.PATH.MAIN, paste("sumo_files", i, sep="_"))
   RANDOM.SEED <- i
   
-  ALGORITHM.NAMES = c('snf', 'lracluster', 'pins', 'mcca', 'nemo', 'sumo') #sumo
-  ALGORITHM.DISPLAY.NAMES = as.list(c('SNF', 'LRAcluster', 'PINS', 'MCCA', 'NEMO', 'SUMO')) #SUMO
+  ALGORITHM.NAMES = c('snf', 'lracluster', 'pins', 'mcca', 'nemo', 'iCluster', 'sumo', 'cimlr')
+  ALGORITHM.DISPLAY.NAMES = as.list(c('SNF', 'LRAcluster', 'PINS', 'MCCA', 'NEMO', 'iClusterBayes', 'SUMO', 'CIMLR'))
   names(ALGORITHM.DISPLAY.NAMES) = ALGORITHM.NAMES
   run.benchmark()
   
